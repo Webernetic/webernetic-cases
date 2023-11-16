042af60007d2c13010d3acb2735e4e7c8ad92eb6
Nikita Korotkevich <kne@webernetic.by>
2023-11-16 16:30:56 +0300

    lidkon case eng and animated

--- lidkon/src/js/main.js
+++ lidkon/src/js/main.js
@@ -1,9 +1,19 @@
-function openDropdown (el) {
-  el.addEventListener("click", (e) => {
-    e.target.classList.toggle("visible");
-  })
-}
-
 document.addEventListener("DOMContentLoaded", function () {
+  const animatedEls = document.querySelectorAll('.animated');
 
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('animate')
+          }            
+      })
+  });
+
+  if (animatedEls.length) {
+      for (let i = 0; i < animatedEls.length; i++) {
+          const elements = animatedEls[i];
+       
+          observer.observe(elements);
+      }    
+  }
 })
