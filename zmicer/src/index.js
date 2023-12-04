50c4cd271417626a44dadd7e934a40b554c938f5
Raman Dziukau <dra@webernetic.by>
2023-12-04 15:41:53 +0300

    feat: added adaptive

--- zmicer/src/index.js
+++ zmicer/src/index.js
@@ -4,7 +4,7 @@ import { setupScrolling } from "./modules/setupScrolling";
 
 document.addEventListener("DOMContentLoaded", () => {
   const resultList = document.querySelector(".result__list");
-  const aboutList = document.querySelector(".about__list");
+  const stackList = document.querySelector(".stack__list");
   const items = {
     skills: ["CSS 3", "Wordpress", "PHP", "javascript", "HTML5", "Figma"],
     directions: [
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ],
   };
 
-  printList(aboutList, items.skills);
+  printList(stackList, items.skills);
   printList(resultList, items.directions);
   setupScrolling(".prototype", ".prototype__scroll");
   setupScrolling(".design", ".design__scroll");
