af5f20cc0f6765cfb781bb697815e0ac5042a7f6
Raman Dziukau <dra@webernetic.by>
2023-12-08 11:59:07 +0300

    feat: added animation

--- LawCompany/src/index.js
+++ LawCompany/src/index.js
@@ -2,15 +2,33 @@ import './index.pug'
 import './index.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
-	const observer = new IntersectionObserver(entries => {
 
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				const elements = entry.target.children
-				for (let i = 0; i < elements.length; i++)
-					elements[i].classList.add('active');
-			}
-		});
-	});
-	observer.observe(document.querySelector('.colors-section__color-items '));
+	const animParams = {
+		homeIcons: document.querySelectorAll(".project-tech__icon"),
+		homeBox: document.querySelectorAll(".project-tech__box"),
+		sectionColors: document.querySelectorAll('.obs'),
+		sectionStage: document.querySelectorAll('.stages-section__stage'),
+		options: {
+			rootMargin: "0px",
+			threshold: 1,
+		},
+		callback: (entries, observer) => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add("isActive");
+					observer.unobserve(entry.target)
+				}
+			})
+		},
+		delayVisible: (elements, delay = 200) => {
+			elements.forEach((icon, i) => setTimeout(() => observerAnim.observe(icon), delay * (i + 1)))
+		},
+	}
+
+	const observerAnim = new IntersectionObserver(animParams.callback, animParams.options);
+
+	animParams.delayVisible(animParams.homeIcons);
+	animParams.delayVisible(animParams.homeBox, 500);
+	animParams.delayVisible(animParams.sectionStage, 300);
+	animParams.delayVisible(animParams.sectionColors, 300);
 })
