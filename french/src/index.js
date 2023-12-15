f260190cd860d140c3c29355e7a0c393f2b215f6
Raman Dziukau <dra@webernetic.by>
2023-12-15 11:55:50 +0300

    feat: add taskSection & animated the section

--- french/src/index.js
+++ french/src/index.js
@@ -1,2 +1,34 @@
 import './index.pug';
 import './index.scss';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const animationParams = {
+        tackImgs: document.querySelectorAll(".task__img"),
+        phoneImg: document.querySelectorAll(".problem__phone"),
+        options: {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5
+        },
+        callback: (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const addAnimtarget = entry.target;
+
+                    addAnimtarget.classList.add('animation');
+
+                    observer.unobserve(addAnimtarget)
+                }
+            });
+        },
+        delayVisible: (elements, delay = 300) => {
+            elements.forEach((icon, i) => setTimeout(() => observer.observe(icon), delay * (i + 1)))
+        },
+    }
+
+    const observer = new IntersectionObserver(animationParams.callback, animationParams.options);
+
+    animationParams.delayVisible(animationParams.phoneImg);
+    animationParams.delayVisible(animationParams.tackImgs);
+})
