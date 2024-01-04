8098b1311695d2fc9d4323ddd1d925ae795f9b62
Demyankov home_PC <demyantaurus@gmail.ru>
2024-01-04 23:33:49 +0300

    wiex: ready desktop version

--- wiex/webpack.config.js
+++ wiex/webpack.config.js
@@ -1,82 +1,83 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const FileManagerPlugin = require("filemanager-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: path.join("js", "main.js"),
+    path: path.join(__dirname, 'dist'),
+    filename: path.join('js', 'main.js'),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.pug$/,
-        loader: "pug-loader",
+        loader: 'pug-loader',
       },
       {
         test: /\.(scss|css)$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
         ],
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
-        test: /\.svg$/,
-        type: "asset/resource",
-        generator: {
-          filename: path.join("assets", "icons", "[name][ext]"),
-        },
+        test: /\.svg?$/i,
+        type: 'asset/resource',
       },
       {
         test: /\.(woff2?|eot|woff|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "./assets/fonts/[name][ext]",
+          filename: './assets/fonts/[name][ext]',
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.pug"),
-      filename: "index.html",
+      template: path.join(__dirname, 'src', 'index.pug'),
+      filename: 'index.html',
     }),
     new FileManagerPlugin({
       events: {
         onStart: {
-          delete: ["dist"],
+          delete: ['dist'],
         },
       },
     }),
     new MiniCssExtractPlugin({
-      filename: path.join("css", "main.css"),
+      filename: path.join('css', 'main.css'),
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/assets/images"),
-          to: path.resolve(__dirname, "dist/assets/images"),
+          from: path.resolve(__dirname, 'src/assets/images'),
+          to: path.resolve(__dirname, 'dist/assets/images'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/icons'),
+          to: path.resolve(__dirname, 'dist/assets/icons'),
         },
       ],
     }),
   ],
   devServer: {
-    watchFiles: path.join(__dirname, "src"),
+    watchFiles: path.join(__dirname, 'src'),
     port: 9000,
   },
   optimization: {
@@ -86,10 +87,10 @@ module.exports = {
           implementation: ImageMinimizerPlugin.imageminMinify,
           options: {
             plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-              ["svgo", { name: "preset-default" }],
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              ['svgo', { name: 'preset-default' }],
             ],
           },
         },
