e47a80f2b817d5e3451ad81e21bd6ab438293f6e
Demyan <demyantaurus@mail.ru>
2024-01-23 15:13:57 +0300

    fix styles and js

--- lottothrill/src/js/showGraphics.js
+++ lottothrill/src/js/showGraphics.js
@@ -1,66 +1,66 @@
-// DESKTOP VERSION
+document.addEventListener('DOMContentLoaded', () => {
+  // DESKTOP VERSION
 
-const btnFirst = document.querySelector('#arrBtn1');
-const fromFirst = document.querySelector('#increaseKeysSection_title');
-const toFirst = document.querySelector('#attendanceSection_title');
-const wrapperFirst = document.querySelector(
-  '#increaseKeysSection_originalsGrahpics',
-);
+  const btnFirst = document.querySelector('#arrBtn1');
+  const fromFirst = document.querySelector('#increaseKeysSection_title');
+  const toFirst = document.querySelector('#attendanceSection_title');
+  const wrapperFirst = document.querySelector(
+    '#increaseKeysSection_originalsGrahpics',
+  );
 
-const btnSecond = document.querySelector('#arrBtn2');
-const fromSecond = document.querySelector('#attendanceSection_title');
-const toSecond = document.querySelector('#attendanceSection_statisticsWrapper');
-const wrapperSecond = document.querySelector(
-  '#attendanceSection_originalsGrahpics',
-);
+  const btnSecond = document.querySelector('#arrBtn2');
+  const fromSecond = document.querySelector('#attendanceSection_title');
+  const toSecond = document.querySelector(
+    '#attendanceSection_statisticsWrapper',
+  );
+  const wrapperSecond = document.querySelector(
+    '#attendanceSection_originalsGrahpics',
+  );
 
-const btnThird = document.querySelector('#arrBtn3');
-const fromThird = document.querySelector('#increaseTrafficSection_title');
-const toThird = document.querySelector('#finalSection');
-const wrapperThird = document.querySelector(
-  '#increaseTrafficSection_originalsGrahpics',
-);
+  const btnThird = document.querySelector('#arrBtn3');
+  const fromThird = document.querySelector('#increaseTrafficSection_title');
+  const toThird = document.querySelector('#finalSection');
+  const wrapperThird = document.querySelector(
+    '#increaseTrafficSection_originalsGrahpics',
+  );
 
-const toggleBlockVisibility = (btn, from, to, block) => {
-  let height = getDistanceBetweenBlocks(from, to);
-  block.style.maxHeight = `${height - 50}px`;
-  toggleActiveClass(btn, 'active');
-  toggleActiveClass(block, 'active');
-};
+  const toggleBlockVisibility = (btn, from, to, block) => {
+    let height = getDistanceBetweenBlocks(from, to);
+    block.style.maxHeight = `${height - 50}px`;
+    toggleActiveClass(btn, 'active');
+    toggleActiveClass(block, 'active');
+  };
 
-btnFirst.addEventListener('click', () =>
-  toggleBlockVisibility(btnFirst, fromFirst, toFirst, wrapperFirst),
-);
+  btnFirst.addEventListener('click', () =>
+    toggleBlockVisibility(btnFirst, fromFirst, toFirst, wrapperFirst),
+  );
 
-btnSecond.addEventListener('click', () =>
-  toggleBlockVisibility(btnSecond, fromSecond, toSecond, wrapperSecond),
-);
+  btnSecond.addEventListener('click', () =>
+    toggleBlockVisibility(btnSecond, fromSecond, toSecond, wrapperSecond),
+  );
 
-btnThird.addEventListener('click', () =>
-  toggleBlockVisibility(btnThird, fromThird, toThird, wrapperThird),
-);
+  btnThird.addEventListener('click', () =>
+    toggleBlockVisibility(btnThird, fromThird, toThird, wrapperThird),
+  );
 
-// MOBILE VERSION
+  // MOBILE VERSION
 
-const btn4 = document.querySelector('#arrBtn4');
-const btn5 = document.querySelector('#arrBtn5');
-const btn6 = document.querySelector('#arrBtn6');
+  const btn4 = document.querySelector('#arrBtn4');
+  const btn5 = document.querySelector('#arrBtn5');
+  const btn6 = document.querySelector('#arrBtn6');
 
-btn4.addEventListener('click', () =>
-  toggleActiveClass(wrapperFirst, 'activeMobile'),
-);
+  btn4.addEventListener('click', () =>
+    toggleActiveClass(wrapperFirst, 'activeMobile'),
+  );
 
-btn5.addEventListener('click', () =>
-  toggleActiveClass(wrapperSecond, 'activeMobile'),
-);
+  btn5.addEventListener('click', () =>
+    toggleActiveClass(wrapperSecond, 'activeMobile'),
+  );
 
-btn6.addEventListener('click', () =>
-  toggleActiveClass(wrapperThird, 'activeMobile'),
-);
-
-const graphWrapper = document.querySelector(
-  '.increaseKeysSection_graphWrapper',
-);
+  btn6.addEventListener('click', () =>
+    toggleActiveClass(wrapperThird, 'activeMobile'),
+  );
+});
 
 const scrollToRight = (element, screenWidth) => {
   let windowWidth = window.innerWidth;
@@ -70,9 +70,12 @@ const scrollToRight = (element, screenWidth) => {
   }
 };
 
-document.addEventListener('DOMContentLoaded', () =>
-  scrollToRight(graphWrapper, 576),
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const graphWrapper = document.querySelector(
+    '.increaseKeysSection_graphWrapper',
+  );
+  scrollToRight(graphWrapper, 576);
+});
 
 const scrollByToRight = (element, screenWidth, step) => {
   let windowWidth = window.innerWidth;
