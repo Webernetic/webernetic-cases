642f63769f387f8d79919dbd9e8f73b556983496
Raman Dziukau <dra@webernetic.by>
2024-02-14 17:40:29 +0300

    feat: added 5 sections

--- parus/src/index.js
+++ parus/src/index.js
@@ -1,1 +1,22 @@
 import './scss/main.scss';
+
+import Swiper from 'swiper';
+import { Scrollbar } from 'swiper/modules';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const swiper = new Swiper('.swiper', {
+        modules: [Scrollbar],
+
+        direction: "vertical",
+        slidesPerView: "auto",
+        freeMode: true,
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+        mousewheel: true,
+    });
+
+})
