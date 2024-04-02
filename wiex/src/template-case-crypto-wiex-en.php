e072221e1ceef8813d1d84dc5f0495b0912a7a65
Demyankov home_PC <demyantaurus@gmail.ru>
2024-04-02 15:55:48 +0300

    Wiex: added English version

---
+++ wiex/src/template-case-crypto-wiex-en.php
@@ -0,0 +1,368 @@
+<?php /* Template Name: Case - Крипта wiex  */ ?>
+
+<? get_header() ?>
+
+<div class="site-container">
+    <main class="main">    
+    <section class="headerSection">
+        <div class="imageWrapper">
+          <div class="container">
+            <div class="wiex"><p>wiex.io</p></div>
+            <h1 class="renovista-title colorInvertText">Promotion of Wiex</h1>
+            <h1 class="renovista-title colorInvertText">crypto exchange</h1>
+            <p class="header_text colorInvertText">
+             Part I. SEO preparation for the launch of a new website
+            </p>
+          </div>
+        </div>
+        <div class="container">
+          <div
+            class="flexContainer headerSection_cardsWrapper headerSection__flexColumn"
+          >
+            <div class="blockWrapper headerSection_card">
+              <h3 class="headerSection__cardTitle">About the project:</h3>
+              <p>
+              The platform allows users to exchange, deposit and withdraw various types of cryptocurrencies.
+              </p>
+              <p>
+              The exchange employs modern security measures, including two-factor authentication, SSL encryption, and cold storage of funds.
+              </p>
+            </div>
+            <div class="flexColumn">
+              <div class="blockWrapper headerSection_card">
+                <h3 class="headerSection__cardTitle">Start of cooperation:</h3>
+                <p>January 2023</p>
+              </div>
+              <div class="blockWrapper headerSection_card">
+                <h3 class="headerSection__cardTitle">Promotion region:</h3>
+                <p>Europe (EN)</p>
+              </div>
+            </div>
+          </div>
+          <div class="container flexContainer headerSection__flexColumn">
+            <div class="blockWrapper headerSection_item colorInvertText">
+              <h3 class="flex-1">Initial status:</h3>
+              <p class="li">The project had no ranking and had not been promoted previously.</p>
+            </div>
+            <div class="blockWrapper headerSection_item colorInvertText">
+              <h3 class="flex-1">Tasks:</h3>
+              <ul class="headerSection_list">
+                <li class="headerSection_itemLine mb-10 colorInvertText li work__item">
+                  <span1 class="headerSection_itemLineNumber">1.</span1>
+                  Increase the site's ranking.
+                </li>
+                <li class="headerSection_itemLine li work__item">
+                  <span1 class="headerSection_itemLineNumber">2.</span1>
+                  Increase traffic.
+                </li>
+              </ul>
+            </div>
+            <div class="blockWrapper headerSection_item colorInvertText">
+              <h3 class="flex-1">Features:</h3>
+              <ul class="headerSection_list">
+                <li class="headerSection_itemLine mb-10 li work__item">
+                  <span1 class="headerSection_itemLineNumber">1.</span1> Intense competition in the field.
+                </li>
+                <li class="headerSection_itemLine li work__item">
+                  <span1 class="headerSection_itemLineNumber">2.</span1> New website and domain.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="container">
+        <section class="structureSection">
+          <h2>Collection of semantics and site structure</h2>
+          <div class="flexContainer structureSection__flexColumn">
+            <div class="flexColumn">
+              <div class="blockWrapper">
+                <p>
+                The principle of collecting semantics is simple: we divided the requests into 2 groups, 
+                informational and commercial <span class="brightText">informational and commercial</span>. The latter are needed to achieve goals. 
+                </p>       
+              </div>
+              <div class="blockWrapper">
+                <p>
+                Then we created a <span class="brightText">new site structure</span> based on the collected semantics.
+                </p>
+              </div>
+            </div>
+            <div class="blockWrapper structureSection_lastBlock">
+              <p class="mb-10">
+              Now we are expanding the semantics of the client's site. We collect queries for informational articles in the blog and the "Knowledge Base" section. 
+              </p>
+              <p>
+              This page contains <span class="brightText">"general queries"</span> for organic traffic. That is, it creates an additional channel for attracting visitors.
+              </p>
+            </div>
+          </div>
+        </section>
+        <div class="profileAnalysisSection">
+          <h2 class="profileAnalysisSection_title">
+          Link profile analysis
+          </h2>
+          <div class="flexContainer profileAnalysisSection__flexColumn">
+            <div>
+              <div class="imageWrapper">
+                <img
+                  src="<?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/chain.png"
+                  alt="Chain"
+                  srcset="
+                  <?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/chain-500.png   500w,
+                  <?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/chain-1200.png 1200w
+                  "
+                  loading="lazy"
+                />
+              </div>
+            </div>
+            <div class="blockWrapper">
+              <p class="mb-10">
+                The site had a small number of external links and limited visibility. 
+              </p>
+              <p class="mb-10">As a result, <span class="brightText">a link building strategy was developed.</p>
+              <p class="mb-10">
+                We used informational blogs and company catalogs. We also took into account the quality of domains and domain zones.
+             </p>
+              <p>
+              Now we plan to gradually <span class="brightText">increase links</span>, taking into account the natural growth and balance of anchor and non-anchor links.
+              </p>
+            </div>
+          </div>
+        </div>
+        <section class="competitorSection">
+          <h2 class="competitorSection_title">Competitor analysis usability</h2>
+          <div class="blockWrapper competitorSection_descriptionWrapper">
+            <p class="mb-10">
+            The client did not have a full-fledged website (except for 1 page). Therefore, the layouts of the future site were analyzed.  
+            </p>
+            <p>
+            <span class="brightText">Improvements</span> were proposed to improve usability.     
+            </p>
+          </div>
+          <ul class="flexContainer competitorSection_list competitorSection__flexColumn">
+            <li class="competitorSection_item">
+              <img
+                class="competitorSection_icon"
+                src="<?php echo THEME_URI ?>/cases/crypto-wiex/assets/icons/globeIcon.svg"
+                alt="Globe"
+              />
+              <p class="competitorSection_itemText">
+              Add the ability to switch the language of the site.
+              </p>
+            </li>
+            <li class="competitorSection_item">
+              <img
+                class="competitorSection_icon"
+                src="<?php echo THEME_URI ?>/cases/crypto-wiex/assets/icons/exchangeIcon.svg"
+                alt="Сurrency exchange"
+              />
+              <p class="competitorSection_itemText">
+              Develop buttons for currency conversion.
+              </p>
+            </li>
+            <li class="competitorSection_item">
+              <img
+                class="competitorSection_icon"
+                src="<?php echo THEME_URI ?>/cases/crypto-wiex/assets/icons/downloadIcon.svg"
+                alt="Loading"
+              />
+              <p class="competitorSection_itemText">
+              Add icons for downloading the mobile application, etc.
+              </p>
+            </li>
+          </ul>
+          <div class="competitorSection_recommendationWrapper">
+            <h2 class="competitorSection_recommendationHeader colorInvertText">
+            Recommendations for website development
+            </h2>
+            <div
+              class="flexContainer competitorSection_contentWrapper competitorSection__flexColumn"
+            >
+              <div class="blockWrapper whiteBg">
+                <p>
+                We have provided the developers with all the necessary <span class="brightText">SEO requirements</span> for the technical optimization of the site. 
+                </p>
+                <p>With these recommendations in mind, the development is now underway.</p>
+              </div>
+              <div class="blockWrapper whiteBg">
+                <img src="<?php echo THEME_URI ?>/cases/crypto-wiex/assets/icons/logo.svg" alt="Логотип" />
+              </div>
+            </div>
+          </div>
+        </section>
+        <div class="optimizationSection">
+          <h2 class="optimizationSection_title">Content optimization</h2>
+          <div class="flexContainer optimizationSection__flexColumn">
+            <div class="optimizationSection_imageWrapper">
+              <img
+                src="<?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/news.png"
+                alt="News"
+                srcset="
+                <?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/news-500.png   500w,
+                <?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/news-1200.png 1200w
+                "
+                loading="lazy"
+              />
+            </div>
+            <div class="blockWrapper">
+              <p class="mb-10">
+                We actively <span class="brightText">work on preparing content</span> for the website. We create a lot of specifications for texts on pages, and the client provides the information. 
+              </p>
+              <p>Content will be placed on the new site.</p>
+            </div>
+          </div>
+          <div class="optimizationSection_footer">
+            <p>
+              We are now actively working on developing and optimizing a new website for the client.
+            </p>
+          </div>
+        </div>
+        <div class="resultSection flexContainer resultSection__flexColumn">
+          <div class="blockWrapper">
+            <h2 class="resultSection_title">Result</h2>
+            <p class="mb-10">
+              Since the client's domain is new and has no history, we focused on building a quality and optimized website with basic SEO requirements in mind. 
+            </p>
+            <p class="mb-10">
+              After the release, expect to see the results of our efforts as the site is ready:
+            </p>
+            <ul>
+              <li class="resultSection_item">
+                attract organic traffic,
+              </li>
+              <li class="resultSection_item">
+                increase positions in search engine results,
+              </li>
+              <li class="resultSection_item">
+                attract more users to the platform.
+              </li>
+            </ul>
+          </div>
+          <div class="resultSection_imageWrapper">
+            <img
+              src="<?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/computer.png"
+              alt="Notebook"
+              srcset="
+              <?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/computer-500.png   500w,
+              <?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/computer-1200.png 1200w
+              "
+              loading="lazy"
+            />
+          </div>
+        </div>
+        <section class="orderSection">
+          <div
+            class="blockWrapper secondaryBg flexContainer orderSection__position"
+          >
+            <div
+              class="blockWrapper whiteBg flex-center orderSection_nameWrapper"
+            >
+              <h4>Аnastasia</h4>
+              <p>ACCOUNT MANAGER</p>
+            </div>
+            <div class="orderSection_imageWrapper">
+              <img
+                src="<?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/girl.png"
+                alt="Girl"
+                srcset="
+                <?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/girl-500.png   500w,
+                <?php echo THEME_URI ?>/cases/crypto-wiex/assets/images/girl-1200.png 1200w
+                " 
+                loading="lazy"
+              />
+            </div>
+            <form class="orderSection_form feedback__form">
+              <h3 class="orderSection_formTitle">Order a consultation</h3>
+              <div class="flexContainer orderSection_inputWrapper">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Phone *"
+                  name="name"
+                /><input
+                  class="input"
+                  type="tel"
+                  placeholder="Phone *"
+                  name="phone"
+                /><input
+                  class="input"
+                  type="email"
+                  placeholder="E-mail *"
+                  name="email"
+                /><input
+                  class="input"
+                  type="url"
+                  placeholder="Website *"
+                  name="site"
+                />
+              </div>
+              <h4 class="orderSection_secondformTitle">How can I help you?</h4>
+              <div class="orderSection_checkboxWrapper">
+                <label class="checkbox"
+                  ><input
+                    class="checkbox"
+                    type="checkbox"
+                    id="site"
+                    name="user_wish[]"
+                    value="Website"
+                  /><span class="checkbox__custom"></span
+                  ><span class="checkbox__value">Website</span></label
+                ><label class="checkbox"
+                  ><input
+                    class="checkbox"
+                    type="checkbox"
+                    id="seo"
+                    name="user_wish[]"
+                    value="SEO"
+                  /><span class="checkbox__custom"></span
+                  ><span class="checkbox__value">SEO</span></label
+                ><label class="checkbox"
+                  ><input
+                    class="checkbox"
+                    type="checkbox"
+                    id="smm"
+                    name="user_wish[]"
+                    value="SMM"
+                  /><span class="checkbox__custom"></span
+                  ><span class="checkbox__value">SMM</span></label
+                ><label class="checkbox"
+                  ><input
+                    class="checkbox"
+                    type="checkbox"
+                    id="revision"
+                    name="user_wish[]"
+                    value="Refinement"
+                  /><span class="checkbox__custom"></span
+                  ><span class="checkbox__value">Refinement</span></label
+                ><label class="checkbox"
+                  ><input
+                    class="checkbox"
+                    type="checkbox"
+                    id="context"
+                    name="user_wish[]"
+                    value="Contect"
+                  /><span class="checkbox__custom"></span
+                  ><span class="checkbox__value">Contect</span></label
+                ><label class="checkbox"
+                  ><input
+                    class="checkbox"
+                    type="checkbox"
+                    id="other"
+                    name="user_wish[]"
+                    value="Other"
+                  /><span class="checkbox__custom"></span
+                  ><span class="checkbox__value">Other</span></label
+                >
+              </div>
+              <div class="orderSection_buttonWrapper">
+                <button class="customButton" type="submit">Send</button>
+              </div>
+            </form>
+          </div>
+        </section>
+      </div>
+    </main>
+</div>
+
+<? get_footer() ?>
