008aa441b27c4c759587e4a976626719a2731b79
Demyankov home_PC <demyantaurus@gmail.ru>
2024-05-17 22:05:07 +0300

    feat: finished cases section

--- design_new/src/js/filtration.js
+++ design_new/src/js/filtration.js
@@ -1,3 +1,4 @@
+// сброс табов фильтров
 const resetFilters = (filterTab) => {
   const wrapper = filterTab.closest('.filtersTabs');
 
@@ -12,6 +13,7 @@ const resetFilters = (filterTab) => {
   });
 };
 
+// переключение табов фильтров
 const toggleActiveFilterName = (filterTab) => {
   filterTab.classList.toggle('active');
 
@@ -28,11 +30,9 @@ const getActiveFilter = (tab) => {
   return activeFilterName;
 };
 
+// отображение отфильтрованных элементов
 const showFilteredElements = (target) => {
-  console.log(target);
-
   let activeFilterName = getActiveFilter(target);
-  console.log(activeFilterName);
   const wrapper = target.closest('.filtersWrapper');
 
   let allElements = wrapper.querySelectorAll('.filterItem');
@@ -57,11 +57,88 @@ const showFilteredElements = (target) => {
   });
 };
 
+// фильтрация
 const filtration = ({ target }) => {
   if (target.classList.contains('filterTab')) {
     toggleActiveFilterName(target);
     showFilteredElements(target);
+
+    const wrapper = document.querySelector('.casesSection_casesWrapper');
+    showMoreButton(wrapper);
   }
+};
+
+// для планшета и мобильного телефона отображаем только 4 карточки
+const hideItemsByDefault = () => {
+  const width = window.innerWidth;
+
+  if (width < 992) {
+    const wrapper = document.querySelector('.casesSection_casesWrapper');
+    wrapper.classList.add('short');
+    controlWrapperHeight(wrapper);
+  }
+};
+
+// обработка отображения кнопки showMore
+const showMoreButton = (wrapper) => {
+  const width = window.innerWidth;
+  const btn = document.querySelector('#showMoreBtn');
+
+  if (width < 992) {
+    const items = wrapper.querySelectorAll('.casesSection_case');
+
+    //находим количество активных карточек
+    let i = 0;
+    items.forEach((item) => {
+      if (!item.classList.contains('disactive')) {
+        i++;
+      }
+    });
+
+    //отображение кнопки
+    if (i < 4) {
+      btn.style.display = 'none';
+    } else {
+      btn.style.display = 'block';
+    }
+  }
+
+  //изменение текста кнопки
+  if (wrapper.classList.contains('short')) {
+    btn.textContent = btn.getAttribute('data-title') || 'Загрузить больше';
+  } else {
+    btn.textContent = btn.getAttribute('data-title-short') || 'Свернуть';
+  }
+};
+
+// controller высоты контейнера с карточками
+const controlWrapperHeight = (wrapper) => {
+  const items = wrapper.querySelectorAll('.casesSection_case');
+
+  let maxHeight = 'none';
+
+  if (wrapper.classList.contains('short')) {
+    let height = 0;
+    let i = 0;
+
+    items.forEach((item) => {
+      if (!item.classList.contains('disactive') && i < 4) {
+        height += item.offsetHeight;
+        i++;
+      }
+    });
+
+    maxHeight = `${height + 3 * 46}px`;
+  }
+
+  wrapper.style.maxHeight = maxHeight;
+};
+
+const showMore = () => {
+  const wrapper = document.querySelector('.casesSection_casesWrapper');
+  wrapper.classList.toggle('short');
+  controlWrapperHeight(wrapper);
+  showMoreButton(wrapper);
 };
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -70,4 +147,10 @@ document.addEventListener('DOMContentLoaded', () => {
   filtersWrappers.forEach((wrapper) =>
     wrapper.addEventListener('click', filtration),
   );
+  const showMoreBtn = document.querySelector('#showMoreBtn');
+  showMoreBtn.addEventListener('click', showMore);
+});
+
+window.addEventListener('load', () => {
+  hideItemsByDefault();
 });
