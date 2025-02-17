935b6b1650c28f02732bc27fa13fe01c9288b3df
Viktarovich Natallia <nata17v@mail.ru>
2025-02-17 09:54:31 +0300

    feat: add adaptive to korex case

--- korex/src/index.js
+++ korex/src/index.js
@@ -1,3 +1,24 @@
 import './scss/main.scss';
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const video = document.querySelector('.finalSection__video');
+  const playButton = document.querySelector('.finalSection__play-btn');
+
+  if (!(video && playButton)) return;
+
+  video.play().catch((err) => console.error('Ошибка автозапуска:', err));
+
+  video.addEventListener('play', () => {
+    playButton.classList.remove('active');
+  });
+
+  video.addEventListener('pause', () => {
+    playButton.classList.add('active');
+  });
+
+  playButton.addEventListener('click', () => {
+    if (video.paused) {
+      video.play().catch((err) => console.error(err));
+    }
+  });
+});
